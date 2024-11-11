14a14ecaeaf490ea5db312317378c56a4248371d
charles-gauthereau <charles.gauthereau@soluce-technologies.com>
2024-11-11 15:31:35 +0100

    Profile Page with update user information.

--- app/(customer)/dashboard/profile/page.tsx
+++ app/(customer)/dashboard/profile/page.tsx
@@ -6,6 +6,11 @@ import {requiredCurrentUser} from "@/auth/current-user";
 import {UserForm} from "@/components/wrappers/Dashboard/Profile/UserForm/UserForm";
 import {prisma} from "@/prisma";
 import {Badge} from "@/components/ui/badge";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+import {ButtonWithConfirm} from "@/components/wrappers/Button/ButtonWithConfirm/ButtonWithConfirm";
+import {ButtonDeleteAccount} from "@/components/wrappers/Dashboard/Profile/ButtonDeleteAccount/ButtonDeleteAccount";
+import {useIsMobile} from "@/hooks/use-mobile";
 
 export default async function RoutePage(props: PageParams<{}>) {
 
@@ -22,23 +27,32 @@ export default async function RoutePage(props: PageParams<{}>) {
 
     console.log(userInfo)
 
+    const test = () => {
+        console.log("test")
+    }
+
     return (
         <Page>
-            <PageHeader>
-                <PageTitle className="flex items-center">
-                    <Avatar className="size-14 mr-3">
-                        <AvatarFallback>{user.name?.[0]}</AvatarFallback>
-                        {user.image ? (
-                            <AvatarImage src={user.image} alt={`${user.name ?? "-"}'s profile picture`}/>
-                        ) : null}
-                    </Avatar>
-                    {user.name}
-                    <Badge className="ml-3">{userInfo.authMethod}</Badge>
-                </PageTitle>
-            </PageHeader>
+            {/*<PageHeader>*/}
+                <div className="justify-between gap-2 sm:flex">
+                    <PageTitle className="flex items-center">
+                        <Avatar className="size-14 mr-3">
+                            <AvatarFallback>{user.name?.[0]}</AvatarFallback>
+                            {user.image ? (
+                                <AvatarImage src={user.image} alt={`${user.name ?? "-"}'s profile picture`}/>
+                            ) : null}
+                        </Avatar>
+                        {user.name}
+                        <Badge className="ml-3 hidden lg:block">{userInfo.authMethod}</Badge>
+                    </PageTitle>
+                    <PageActions className={"mt-2"}>
+                        <ButtonDeleteAccount/>
+                    </PageActions>
+                </div>
+            {/*</PageHeader>*/}
             <PageContent>
-                <UserForm userId={userInfo.id} defaultValues={userInfo} />
+                <UserForm userId={userInfo.id} defaultValues={userInfo}/>
             </PageContent>
         </Page>
-)
+    )
 }
---
+++ src/components/wrappers/Button/ButtonWithConfirm/ButtonWithConfirm.tsx
@@ -0,0 +1,45 @@
+"use client"
+import {Button} from "@/components/ui/button";
+import {useState} from "react";
+import {Loader2} from "lucide-react";
+
+export type VariantButton = {
+    secondary: string
+    default: string
+    outline: string
+    ghost: string
+    link: string
+    destructive: string
+}
+
+export type ButtonWithConfirmProps = {
+    icon?: any,
+    text: string,
+    variant?:  keyof VariantButton ,
+    className?: string,
+    onClick?: () => void,
+    isPending? : boolean
+};
+
+export const ButtonWithConfirm = (props: ButtonWithConfirmProps) => {
+    const [isConfirming, setIsConfirming] = useState(false)
+    return(
+        <Button
+            onClick={() => {
+                if (isConfirming) {
+                    props.onClick()
+                } else {
+                    setIsConfirming(true);
+                }
+            }}
+            variant={props.variant ? props.variant : "default"}
+            className={props.className}
+        >
+            {props.isPending && <Loader2 className="animate-spin mr-4" size={16}/>}
+            {isConfirming ? "Are you sure ?" : `${props.text}`}
+            <>
+                {props.icon ? props.icon : null}
+            </>
+        </Button>
+    )
+}
