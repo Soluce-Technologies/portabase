85ecfed50d8add95c4844ad30e89fda92113645d
charles-gauthereau <charles.gauthereau@soluce-technologies.com>
2024-11-17 11:37:23 +0100

    Working on settings Page. Adding init function on next.js startup.

--- app/(customer)/dashboard/layout.tsx
+++ app/(customer)/dashboard/layout.tsx
@@ -12,13 +12,17 @@ export default async function Layout({children}: { children: React.ReactNode }) 
 
     const user = await currentUser()
 
-    const userInfo = await prisma.user.findUnique({
-        where: {
-            email: user.email
-        }
-    })
+    if(user){
+        const userInfo = await prisma.user.findUnique({
+            where: {
+                email: user?.email
+            }
+        })
 
-    if (!userInfo) redirect('/login')
+        if(userInfo){
+            redirect('/login')
+        }
+    }
 
     return (
         <SidebarProvider>
