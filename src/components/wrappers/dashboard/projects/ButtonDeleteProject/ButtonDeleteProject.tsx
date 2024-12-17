43161b8523730381868e4d0d7355230c144c9c0a
charles-gauthereau <charles.gauthereau@soluce-technologies.com>
2024-12-17 19:42:02 +0100

    Project delete.

--- src/components/wrappers/dashboard/projects/ButtonDeleteProject/ButtonDeleteProject.tsx
+++ src/components/wrappers/dashboard/projects/ButtonDeleteProject/ButtonDeleteProject.tsx
@@ -2,29 +2,36 @@
 
 import {Trash2} from "lucide-react";
 import {ButtonWithConfirm} from "@/components/wrappers/common/button/button-with-confirm";
+import {useMutation} from "@tanstack/react-query";
+import {deleteProjectAction} from "@/components/wrappers/dashboard/projects/ButtonDeleteProject/delete-project.action";
+import {useRouter} from "next/navigation";
+import {toast} from "sonner";
 
 export type ButtonDeleteProjectProps = {
     text? : string
+    projectId: string
 }
 
 export const ButtonDeleteProject = (props: ButtonDeleteProjectProps) => {
-
-    // const mutation = useMutation({
-    //     mutationFn: () => deleteUserAction(""),
-    //     onSuccess: async () => {
-    //         await signOutAction();
-    //     },
-    // })
+    const router = useRouter()
+    const mutation = useMutation({
+        mutationFn: () => deleteProjectAction(props.projectId),
+        onSuccess: async (result) => {
+            router.push("/dashboard/projects")
+            if(result.data.success) {
+                toast.success(result.data.actionSuccess.message);
+            }
+        },
+    })
 
     return (
         <ButtonWithConfirm
             text={props.text ? props.text : ""}
             onClick={() => {
-                // mutation.mutate()
-                console.log("ok")
+                mutation.mutate()
             }}
             variant={"destructive"}
-            // isPending={mutation.isPending}
+            isPending={mutation.isPending}
             className="gap-2"
             icon={<Trash2/>}
         />
