4448c9e1662fd0f90359828f4c1d5e80ed991906
charlesgauthereau <charles.gauthereau@soluce-technologies.com>
2025-08-28 09:23:04 +0200

    Working on the retention system.

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -33,6 +33,7 @@ async function createSettingsIfNotExist() {
         smtpPort: env.SMTP_PORT ?? null,
         smtpUser: env.SMTP_USER ?? null,
         s3EndPointUrl: env.S3_ENDPOINT ?? null,
+        // gitleaks:allow
         s3AccessKeyId: env.S3_ACCESS_KEY ?? null,
         s3SecretAccessKey: env.S3_SECRET_KEY ?? null,
         S3BucketName: env.S3_BUCKET_NAME ?? null,
