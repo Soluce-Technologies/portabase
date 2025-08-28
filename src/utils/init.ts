598eb4495272641f64a762a29bf77d4c15b75363
charlesgauthereau <charles.gauthereau@soluce-technologies.com>
2025-08-28 09:31:57 +0200

    Working on the retention system.

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -36,6 +36,7 @@ async function createSettingsIfNotExist() {
         s3AccessKeyId: env.S3_ACCESS_KEY ?? null,
         s3SecretAccessKey: env.S3_SECRET_KEY ?? null,
         S3BucketName: env.S3_BUCKET_NAME ?? null,
+
     };
 
     const [existing] = await db.select().from(drizzleDb.schemas.setting).where(eq(drizzleDb.schemas.setting.name, "system")).limit(1);
